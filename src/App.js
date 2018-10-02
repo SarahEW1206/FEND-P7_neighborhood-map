5d37846bccc3022db96dc15963a99cf18782b578
Sarah Waldie <sarah@sassafrasbiz.com>
2018-10-02 14:57:20 -0400

    Added map styling to go with theme.

--- src/App.js
+++ src/App.js
@@ -143,6 +143,86 @@ class App extends Component {
           this.map = new google.maps.Map(document.getElementById('map'), {
             center: amalie,
             zoom: 15,
+            styles: [
+              {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                  {
+                    "color": "#444444"
+                  }
+                ]
+              },
+              {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                  {
+                    "color": "#f2f2f2"
+                  }
+                ]
+              },
+              {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                  {
+                    "saturation": -100
+                  },
+                  {
+                    "lightness": 45
+                  }
+                ]
+              },
+              {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                  {
+                    "visibility": "simplified"
+                  }
+                ]
+              },
+              {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              },
+              {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                  {
+                    "color": "#002868"
+                  },
+                  {
+                    "visibility": "on"
+                  }
+                ]
+              }
+            ]
           });
           this.infowindow = new google.maps.InfoWindow();
 
